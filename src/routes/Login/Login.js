46c924cbafc07a7cc0061052d03a6662aa196b67
Becki Blumer <rblumer13@gmail.com>
2019-12-11 18:41:07 -0600

    changes to Login component

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -10,7 +10,7 @@ class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'gfremont@email.com',
+      username: 'teacher@email.com',
       password: 'Password123!',
       error: null,
       username_error: null,
