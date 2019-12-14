c79a2e6ce3ff71d7ca1b558e557602246de85287
Becki Blumer <rblumer13@gmail.com>
2019-12-13 21:39:36 -0600

    updated API_BASE_URL due to errors with fetch requests

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   CI: process.env.REACT_APP_CI,
-  API_BASE_URL: "https://protected-journey-28335.herokuapp.com/api/",
+  API_BASE_URL: "https://protected-journey-28335.herokuapp.com/api",
   TOKEN_KEY: 'ducklings-key',
 }
