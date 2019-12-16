31fec49efd68f27bcd2982983872527f305cd3a3
Becki Blumer <rblumer13@gmail.com>
2019-12-16 04:58:07 -0600

    updated config file to use heroku for base URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   CI: process.env.REACT_APP_CI,
-  // API_BASE_URL: "https://protected-journey-28335.herokuapp.com/api",
-  API_BASE_URL: "http://localhost:8000/api",
+  API_BASE_URL: "https://protected-journey-28335.herokuapp.com/api",
+  // API_BASE_URL: "http://localhost:8000/api",
   TOKEN_KEY: 'ducklings-key',
 }
